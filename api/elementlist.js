7018305b955db49e0d281b42c7b99515deb14c87
coderstephen <me@stephencoakley.com>
2015-06-15 15:40:02 -0500

    Add API docs with ApiGen

---
+++ api/elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","Windows\\Registry\\Exception"],["c","Windows\\Registry\\InvalidTypeException"],["c","Windows\\Registry\\KeyNotFoundException"],["c","Windows\\Registry\\OperationFailedException"],["c","Windows\\Registry\\Registry"],["c","Windows\\Registry\\RegistryHandle"],["c","Windows\\Registry\\RegistryKey"],["c","Windows\\Registry\\RegistryKeyIterator"],["c","Windows\\Registry\\RegistryValueIterator"],["c","Windows\\Registry\\ValueNotFoundException"]];
